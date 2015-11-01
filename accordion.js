bca79297e3055234fbd4d5a6ee2208beec16a1ab
Markus Falk <mail@markus-falk.com>
2015-11-01 12:14:41 +0100

    [FEATURE] add docs

--- accordion.js
+++ accordion.js
@@ -2,59 +2,91 @@ define(['jquery', 'jquery.exists'], function($) {
 
   'use strict';
 
-  /************************************************************
-    @author mail@markus-falk.com
-    @description simple and accessible accordion
-    @requires jQuery 1.7+
-  *************************************************************/
+  /*
+   * simple and accessible accordion
+   * @author mail@markus-falk.com
+   * @requires jQuery 1.7+
+   */
 
   var Accordion = {
+
+    /**
+     * Defaults to be extended into options
+     */
     DEFAULTS: {
       animationSpeed: 300,
       naturalBehavior: false
     },
-    cacheElements: function() {
+
+    /**
+     * Cache jQuery elements.
+     * @function _cacheElements
+     * @private
+     */
+    _cacheElements: function() {
       this.$accordion = $('.accordion');
       this.$accordion_content = $('.accordion-content');
       this.$accordion_header = $('.accordion-header');
       this.$accordion_opened = $('.accordion-opened');
     },
 
+    /**
+     * Initialize the accordion.
+     * @function init
+     * @public
+     * @param {Object} options - The properties you would like to overwrite.
+     */
     init: function(options) {
-      Accordion.cacheElements();
+      Accordion._cacheElements();
       Accordion.$accordion.exists(function() {
-        Accordion.setOptions(options);
-        Accordion.addARIAlabels();
-        Accordion.setupAccordion();
-        Accordion.bindEvents();
-        Accordion.openAccordionViaClass();
-        Accordion.openAccordionViaHash();
+        Accordion._setOptions(options);
+        Accordion._addARIAlabels();
+        Accordion._setupAccordion();
+        Accordion._bindEvents();
+        Accordion._openAccordionViaClass();
+        Accordion._openAccordionViaHash();
       });
     },
 
-    bindEvents: function() {
+    /**
+     * Bind events to all interactive elements.
+     * @function _bindEvents
+     * @private
+     */
+    _bindEvents: function() {
+
       // Click accordion header
       this.$accordion_header.on('click', function(event) {
         event.preventDefault();
-        Accordion.closeAll($(this));
-        Accordion.toggleAccordion($(this));
+        Accordion._closeAll($(this));
+        Accordion._toggleAccordion($(this));
       });
 
       // tab to accordion header and press space bar
       this.$accordion_header.on('keydown', function(event) {
         if (event.keyCode === 32) {
           event.preventDefault();
-          Accordion.toggleAccordion($(this));
+          Accordion._toggleAccordion($(this));
         }
       });
     },
 
-    setOptions: function(options) {
+    /**
+     * Extend defaults with customized options.
+     * @function _setOptions
+     * @private
+     */
+    _setOptions: function(options) {
       // extend DEFAULTS with given options
       this.options = $.extend({}, Accordion.DEFAULTS, options);
     },
 
-    setupAccordion: function() {
+    /**
+     * Setup HTML and trigger custom events.
+     * @function _setupAccordion
+     * @private
+     */
+    _setupAccordion: function() {
       this.$accordion.attr('role', 'tablist');
       this.$accordion_content.attr('aria-expanded', 'false').attr('role', 'tabpanel').hide();
       this.$accordion_header.attr('role', 'tab').attr('tabindex', '0').attr('aria-selected', 'false');
@@ -64,7 +96,12 @@ define(['jquery', 'jquery.exists'], function($) {
       });
     },
 
-    addARIAlabels: function() {
+    /**
+     * Add ARIA labels to each accordion.
+     * @function _addARIAlabels
+     * @private
+     */
+    _addARIAlabels: function() {
       this.$accordion.each(function(index) {
 
         var
@@ -98,17 +135,29 @@ define(['jquery', 'jquery.exists'], function($) {
       });
     },
 
-    closeAll: function(accordion_header) {
+    /**
+     * Close all open accordion entries within one accordion for natural behavior.
+     * @function _closeAll
+     * @private
+     * @param {Object} accordion_header - The clicked accordion header.
+     */
+    _closeAll: function(accordion_header) {
 
       var opened = accordion_header.closest('.accordion').find('.accordion-active');
 
       // close opened entry when option is set and the open entry is not the clicked
       if(this.options.naturalBehavior && !opened.is(accordion_header)) {
-        this.closeAccordion(opened);
+        this._closeAccordion(opened);
       }
     },
 
-    openAccordion: function(accordion_header) {
+    /**
+     * Open a single accordion entry and trigger events.
+     * @function _openAccordion
+     * @private
+     * @param {Object} accordion_header - The clicked accordion header.
+     */
+    _openAccordion: function(accordion_header) {
 
       var
       accordion_content = accordion_header.next();
@@ -125,7 +174,13 @@ define(['jquery', 'jquery.exists'], function($) {
 
     },
 
-    closeAccordion: function(accordion_header) {
+    /**
+     * Close a single accordion entry and trigger events.
+     * @function _closeAccordion
+     * @private
+     * @param {Object} accordion_header - The clicked accordion header.
+     */
+    _closeAccordion: function(accordion_header) {
 
       var
       accordion_content = accordion_header.next();
@@ -142,18 +197,30 @@ define(['jquery', 'jquery.exists'], function($) {
 
     },
 
-    toggleAccordion: function(accordion_header) {
+    /**
+     * Returns the sum of arg1 and arg1.
+     * @function _toggleAccordion
+     * @private
+     * @param {Object} accrodion_header - The clicked accordion header.
+     */
+    _toggleAccordion: function(accordion_header) {
 
       if (accordion_header.attr('aria-selected') === 'false') {
         // open:
-        this.openAccordion(accordion_header);
+        this._openAccordion(accordion_header);
       } else {
         // close:
-        this.closeAccordion(accordion_header);
+        this._closeAccordion(accordion_header);
       }
     },
 
-    openAccordionViaHash: function() {
+    /**
+     * Open a single accordion entry via a hash in the URL.
+     * @function _openAccordionViaHash
+     * @private
+     * @param {Object} accrodion_header - The clicked accordion header.
+     */
+    _openAccordionViaHash: function() {
       // find linked accordion content and click
       // corresponding .accordion-header to open it
       if(window.location.hash) {
@@ -161,7 +228,12 @@ define(['jquery', 'jquery.exists'], function($) {
       }
     },
 
-    openAccordionViaClass: function() {
+    /**
+     * Returns the sum of arg1 and arg1.
+     * @function _openAccordionViaClass
+     * @private
+     */
+    _openAccordionViaClass: function() {
       // open accordion content with class
       // 'accordion-opened'
       this.$accordion_opened.exists(function() {
@@ -177,8 +249,11 @@ define(['jquery', 'jquery.exists'], function($) {
     }
   };
 
-  return {
-    init: Accordion.init
+  return /** @alias module:Accordion */ {
+    /** init */
+    init: Accordion.init,
+    /** destroy */
+    destroy: Accordion.destroy
   };
 
 });
