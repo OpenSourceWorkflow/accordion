c18cf0fade9793e4886f43b0a25a2ec0538e688a
Markus Falk <mail@markus-falk.com>
2015-11-01 13:53:36 +0100

    [FEATURE] add destroy function and avoid multiple inits

--- accordion.js
+++ accordion.js
@@ -40,45 +40,10 @@ define(['jquery', 'jquery.exists'], function($) {
       Accordion._cacheElements();
       Accordion.$accordion.exists(function() {
         Accordion._setOptions(options);
-        Accordion._addARIAlabels();
         Accordion._setupAccordion();
-        Accordion._bindEvents();
         Accordion._openAccordionViaClass();
         Accordion._openAccordionViaHash();
       });
-    },
-
-    /**
-     * Bind events to all interactive elements.
-     * @function _bindEvents
-     * @private
-     */
-    _bindEvents: function() {
-
-      // Click accordion header
-      this.$accordion_header.on('click', function(event) {
-        event.preventDefault();
-        Accordion._closeAll($(this));
-        Accordion._toggleAccordion($(this));
-      });
-
-      // tab to accordion header and press space bar
-      this.$accordion_header.on('keydown', function(event) {
-        if (event.keyCode === 32) {
-          event.preventDefault();
-          Accordion._toggleAccordion($(this));
-        }
-      });
-    },
-
-    /**
-     * Extend defaults with customized options.
-     * @function _setOptions
-     * @private
-     */
-    _setOptions: function(options) {
-      // extend DEFAULTS with given options
-      this.options = $.extend({}, Accordion.DEFAULTS, options);
     },
 
     /**
@@ -87,52 +52,131 @@ define(['jquery', 'jquery.exists'], function($) {
      * @private
      */
     _setupAccordion: function() {
+
+      // set html attributes
       this.$accordion.attr('role', 'tablist');
       this.$accordion_content.attr('aria-expanded', 'false').attr('role', 'tabpanel').hide();
       this.$accordion_header.attr('role', 'tab').attr('tabindex', '0').attr('aria-selected', 'false');
 
-      this.$accordion.each(function() {
-        $(this).trigger('accordion.initialized');
+      // set up each accordion
+      this.$accordion.each(function(index) {
+        var $this = $(this);
+
+        if(!$this.data('name')) {
+
+          // save state
+          $this.data('name', 'accordion');
+
+          // setup
+          Accordion._addARIAlabels($this, index);
+          Accordion._bindEvents($this);
+
+          // publish event with current accordion
+          $this.trigger('accordion.initialized', $this);
+
+        }
       });
+    },
+
+    /**
+     * Initialize the accordion.
+     * @function destroy
+     * @public
+     */
+    destroy: function() {
+
+      Accordion.$accordion.removeAttr('role');
+      Accordion.$accordion.data('name', null);
+
+      Accordion.$accordion_header.removeAttr('id');
+      Accordion.$accordion_header.removeAttr('role');
+      Accordion.$accordion_header.removeAttr('tabindex');
+      Accordion.$accordion_header.removeAttr('aria-selected');
+      Accordion.$accordion_header.removeAttr('aria-controls');
+      Accordion.$accordion_header.removeClass('accordion-active');
+
+      Accordion.$accordion_content.removeAttr('aria-expanded');
+      Accordion.$accordion_content.removeAttr('role');
+      Accordion.$accordion_content.removeAttr('aria-hidden');
+      Accordion.$accordion_content.removeAttr('aria-labelledby');
+      Accordion.$accordion_content.removeAttr('style');
+      Accordion.$accordion_content.removeAttr('id');
+      Accordion.$accordion_content.removeClass('accordion-opened');
+
+      Accordion.$accordion_header.off('click keydown');
+
+    },
+
+    /**
+     * Bind events to all interactive elements.
+     * @function _bindEvents
+     * @param {Object} accordion - Current accordion from setup loop.
+     * @private
+     */
+    _bindEvents: function(accordion) {
+
+      // Click accordion header
+      accordion.find('.accordion-header').on('click', function(event) {
+        event.preventDefault();
+        Accordion._closeAll($(this));
+        Accordion._toggleAccordion($(this));
+      });
+
+      // tab to accordion header and press space bar
+      accordion.find('.accordion-header').on('keydown', function(event) {
+        if (event.keyCode === 32) {
+          event.preventDefault();
+          Accordion._toggleAccordion($(this));
+        }
+      });
+
+    },
+
+    /**
+     * Extend defaults with customized options.
+     * @function _setOptions
+     * @param {Object} options - .
+     * @private
+     */
+    _setOptions: function(options) {
+      // extend DEFAULTS with given options
+      this.options = $.extend({}, Accordion.DEFAULTS, options);
     },
 
     /**
      * Add ARIA labels to each accordion.
      * @function _addARIAlabels
+     * @param {Object} accordion - jQuery object with current accordion from setup loop.
+     * @param {Object} index - index from setup loop.
      * @private
      */
-    _addARIAlabels: function() {
-      this.$accordion.each(function(index) {
+    _addARIAlabels: function(accordion, index) {
 
-        var
-        that = $(this),
-        $accordion_header = that.find('.accordion-header').attr('id', 'accordion-' + index),
-        $accordion_content = that.find('.accordion-content').attr('id', 'accordion-' + index);
+      var
+      $accordion_header = accordion.find('.accordion-header').attr('id', 'accordion-' + index),
+      $accordion_content = accordion.find('.accordion-content').attr('id', 'accordion-' + index);
 
-        $accordion_header.each(function (index){
+      $accordion_header.each(function (index){
 
-          var that = $(this);
+        var that = $(this);
 
-          // set IDs
-          that
-            .attr('id', that.attr('id') + '-header-' + index)
-            .next()
-            .attr('id', that.next().attr('id') + '-content-' + index);
+        // set IDs
+        that
+          .attr('id', that.attr('id') + '-header-' + index)
+          .next()
+          .attr('id', that.next().attr('id') + '-content-' + index);
 
-          // set aria-controls
-          that
-            .attr('aria-controls', that.next().attr('id'));
-        });
-
-        $accordion_content.each(function() {
-          var that = $(this);
-
-          // set aria-labelledby
-          that
-            .attr('aria-labelledby', that.prev().attr('id'));
-        });
-
+        // set aria-controls
+        that.attr('aria-controls', that.next().attr('id'));
       });
+
+      $accordion_content.each(function() {
+        var that = $(this);
+
+        // set aria-labelledby
+        that.attr('aria-labelledby', that.prev().attr('id'));
+      });
+
     },
 
     /**
@@ -198,7 +242,7 @@ define(['jquery', 'jquery.exists'], function($) {
     },
 
     /**
-     * Returns the sum of arg1 and arg1.
+     * Toggle the accordion entry.
      * @function _toggleAccordion
      * @private
      * @param {Object} accrodion_header - The clicked accordion header.
@@ -229,7 +273,7 @@ define(['jquery', 'jquery.exists'], function($) {
     },
 
     /**
-     * Returns the sum of arg1 and arg1.
+     * If a hash is provided that matches the accordion header's ID this entry is opened.
      * @function _openAccordionViaClass
      * @private
      */
